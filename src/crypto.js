28273425fabbc8b9db5f70463e9a524cc9f5763f
/shynox <66389292+BilalTaner@users.noreply.github.com>
2021-02-06 19:11:35 +0300

    Update crypto.js

--- src/crypto.js
+++ src/crypto.js
@@ -13,21 +13,21 @@ module.exports = {
       const response2 = await request(url2);
       const $ = cheerio.load(response);
       const $2 = cheerio.load(response2);
-      let nameandtag = $('div[class="currency-card relative bg-blue-gray-9 rounded-md p-16"]').find('div[class="page-title text-white"]').text()
+      let nameandtag = $('div[class="page-title text-white"]').text()
       let name = nameandtag.substring(0, nameandtag.indexOf(' ('))
       var tag = nameandtag.substring(nameandtag.indexOf('(') + 1, nameandtag.indexOf(')'));
-      let volume = $2('div[class="sc-57oli2-0 dEqHl cmc-body-wrapper"]').find('div[class="sc-16r8icm-0 iTsqJR statsSection___2aZ29"]').find('div[class="statsValue___2iaoZ"]').eq(2).text()
-      let marketcap = $2('div[class="sc-1xxpehh-1 dXdBcN"]').find('div[class="sc-16r8icm-0 iTsqJR statsSection___2aZ29"]').find('div[class="statsItemRight___yJ5i-"]').children('div').eq(0).text()
-      let change = $('div[class="currency-card relative bg-blue-gray-9 rounded-md p-16"]').find('div[class="flex justify-between mt-8"]').children('div').children('div').children('div').eq(0).text()
-      let date = $('div[class="currency-card relative bg-blue-gray-9 rounded-md p-16"]').find('div[class="text-xs text-blue-gray-2"]').eq(0).text()
-      let interval = $('div[class="currency-card relative bg-blue-gray-9 rounded-md p-16"]').find('div[class="flex justify-between mt-8"]').find('div[class="flex justify-between"]').find('div[class="text-md font-semibold text-white mt-4"]').eq(2).text()
-      let usd = $2('div[class="sc-16r8icm-0 hKZfDX container"]').find('div[class="sc-16r8icm-0 fIhwvd"]').children('table').children('tbody').children('tr').eq(0).children('td').text()
-      let tl = $('div[class="currency-card relative bg-blue-gray-9 rounded-md p-16"]').find('div[class="flex justify-between"]').find('div[class="mr-16"]').eq(0).find('div[class="text-md font-semibold text-white mt-4"]').text()
+      let volume = $2('div[class="statsValue___2iaoZ"]').eq(2).text()
+      let marketcap = $2('div[class="sc-16r8icm-0 iTsqJR statsSection___2aZ29"]').eq(0).eq(0).children('div').eq(0).children('div').eq(0).children('div').eq(0).children('div').eq(1).text()
+      let change = $('div[class="flex justify-between mt-8"]').children('div').children('div').children('div').eq(0).text()
+      let date = $('div[class="text-xs text-blue-gray-2"]').eq(0).text()
+      let interval = $('div[class="flex justify-between"]').find('div[class="text-md font-semibold text-white mt-4"]').eq(2).text()
+      let usd = $2('div[class="sc-16r8icm-0 fIhwvd"]').children('table').children('tbody').children('tr').eq(0).children('td').text()
+      let tl = $('div[class="flex justify-between"]').find('div[class="mr-16"]').eq(0).find('div[class="text-md font-semibold text-white mt-4"]').text()
       var person = {
         name: name,
         tag: tag,
         volume: formatNumber(volume),
-        marketcap: formatNumber(marketcap),
+        marketcap: formatNumber(marketcap.replace('%', '')),
         date: getDate(date.replace('Son (', '').replace(')', '')),
         daily: info = {
           change: formatNumber(change || '0'),
